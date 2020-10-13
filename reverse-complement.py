6e5c54559c8707c2875b91706c08d9442072a388
alquicir <72780339+alquicir@users.noreply.github.com>
2020-10-12 23:38:12 -0500

    Update reverse-complement.py

--- reverse-complement.py
+++ reverse-complement.py
@@ -6,7 +6,7 @@ VERSION
     1.0
     
 AUTHOR
-	Hely Salgado, Carlos Estrada, Shirley Alquicira
+	Hely Salgado, Carlos Estrada, Shirley Alquicira.
     
 DESCRIPTION
 	Make the reverse complement of DNA sequence
